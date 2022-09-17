cc060d98e31e61d54de65ac1e6194ac57bdc3440
ryanpfalz <19982322+ryanpfalz@users.noreply.github.com>
2022-09-17 15:16:31 -0500

    updated to use object id

--- infra/setup/setupKeyVault.ps1
+++ infra/setup/setupKeyVault.ps1
@@ -6,8 +6,8 @@ $keyVaultName = "subautokv"
 
 New-AzKeyVault -Name $keyVaultName -ResourceGroupName $resourceGroup -Location $location
 
-$clientId = "<YOUR-SERVICEPRINCIPAL-OBJECTID>"
-Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -UserPrincipalName $clientId -PermissionsToSecrets get,set
+$objectId = "<YOUR-SERVICEPRINCIPAL-OBJECTID>"
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -UserPrincipalName $objectId -PermissionsToSecrets get,set
 
 $secretvalue = ConvertTo-SecureString "<YOUR-SERVICEPRINCIPAL-SECRET>" -AsPlainText -Force
 $secret = Set-AzKeyVaultSecret -VaultName $keyVaultName -Name "SpSecret" -SecretValue $secretvalue
