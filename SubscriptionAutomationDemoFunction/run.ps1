a4ed85a10dc5490d669ec2b831a79d0db0ef20b6
ryanpfalz <19982322+ryanpfalz@users.noreply.github.com>
2022-09-18 11:43:17 -0500

    updated comments

--- SubscriptionAutomationDemoFunction/run.ps1
+++ SubscriptionAutomationDemoFunction/run.ps1
@@ -6,15 +6,12 @@ param($Request, $TriggerMetadata)
 # Global error action preference to enable exception handling if Az command fails
 $ErrorActionPreference = "Stop"
 
-# Authenticate with SP
-[SecureString]$pwd = [Environment]::GetEnvironmentVariable("APP_SEC") | ConvertTo-SecureString -AsPlainText -Force
-[PSCredential]$credentials = New-Object System.Management.Automation.PSCredential ([Environment]::GetEnvironmentVariable('APP_ID'), $pwd)
+# Authenticate with env variables
+[SecureString]$sec = [Environment]::GetEnvironmentVariable("APP_SEC") | ConvertTo-SecureString -AsPlainText -Force
+[PSCredential]$credentials = New-Object System.Management.Automation.PSCredential ([Environment]::GetEnvironmentVariable('APP_ID'), $sec)
 $tenantId = [Environment]::GetEnvironmentVariable('APP_TENANT')
 
 Connect-AzAccount -ServicePrincipal -Credential $credentials -Tenant $tenantId
-
-# Import-Module "./SubscriptionAutomationDemoFunction/modules/Az.Accounts/2.7.2/Az.Accounts.psm1"
-# Import-Module "./SubscriptionAutomationDemoFunction/modules/Az.Subscription/0.8.1/Az.Subscription.psm1"
 
 # Interact with query parameters or the body of the request.
 $billingAccount = $Request.Body.BillingAccount
@@ -22,23 +19,18 @@ $enrollmentAccount = $Request.Body.EnrollmentAccount
 $subscriptionAlias = $Request.Body.SubscriptionAlias
 $subscriptionName = $Request.Body.SubscriptionName
 
-# use https://learn.microsoft.com/en-us/powershell/azure/authenticate-azureps?view=azps-8.3.0, get credentials from KV
-
-# Check for parameters and create subscription
+# Validate parameters
 if (!$billingAccount -or !$enrollmentAccount -or !$subscriptionAlias -or !$subscriptionName) {
     $body = "Please provide a billing account, enrollment account, subscription alias, and subscription name."
 } else {
 
+    # Create subscription
     try {
 
-        $body = "Received arguments:
-        Billing Account: $billingAccount
-        Enrollment Account: $enrollmentAccount
-        Subscription Alias: $subscriptionAlias
-        Subscription Name: $subscriptionName"
+        $body = "Received arguments:`nBilling Account: $billingAccount`nEnrollment Account: $enrollmentAccount`nSubscription Alias: $subscriptionAlias`nSubscription Name: $subscriptionName"
         
-        # This command will create a subscription
-        New-AzSubscriptionAlias -AliasName $subscriptionAlias -SubscriptionName $subscriptionName -BillingScope "/providers/Microsoft.Billing/BillingAccounts/$billingAccount/enrollmentAccounts/$enrollmentAccount"
+        # TODO This command will create a subscription
+        # New-AzSubscriptionAlias -AliasName $subscriptionAlias -SubscriptionName $subscriptionName -BillingScope "/providers/Microsoft.Billing/BillingAccounts/$billingAccount/enrollmentAccounts/$enrollmentAccount"
 
     } catch {
         $body = "An error occurred: $_"
