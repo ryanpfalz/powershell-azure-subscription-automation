afb94fe982c677d2a690a53df8f51bc0b76e2c61
ryanpfalz <19982322+ryanpfalz@users.noreply.github.com>
2022-09-18 13:47:03 -0500

    created logic app workflow

--- infra/deploy/deployLogicApp.ps1
+++ infra/deploy/deployLogicApp.ps1
@@ -5,7 +5,7 @@ $logicAppName = "subautodemologic"
 # Create Resource Group
 New-AzResourceGroup -Name $resourceGroup -Location $location
 
-# Create Logic App
+# Create Logic App #TODO DEPLOY CONSUMPTION
 New-AzLogicApp -ResourceGroupName $resourceGroup -Location $location -Name $logicAppName -DefinitionFilePath "./infra/deploy/SubscriptionAutomationDemoLogicApp/workflow.json"
 
 # Update Logic App
